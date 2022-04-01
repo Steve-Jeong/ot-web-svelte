39e08b2bd50704fab6d44bac30e78a1b7c035c74
Steve Jeong <jst0930@gmail.com>
2022-04-01 20:17:28 +0300

    first

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+
+<h1><a href='/'>WEB</a></h1>
+
+<p>Web Dictionary</p>
+
+<slot></slot>
+
+<p>(c) All Rights Reserved</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  let content = ''
+
+</script>
+
+<ul>
+  <li><a href="/dict/html">HTML</a></li>
+  <li><a href="/dict/css">CSS</a></li>
+  <li><a href="/dict/javascript">JavaScript</a></li>
+</ul>
+
+<h2>Welcome</h2>
+<p>Welcome to the WEB Dictionary</p>
+

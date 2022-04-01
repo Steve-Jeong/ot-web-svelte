d1eeecfb9ed253f24b779cdd00ce2a9d6a71ceaf
Steve Jeong <jst0930@gmail.com>
2022-04-01 20:33:58 +0300

    id page added

---
+++ src/routes/dict/[id].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export function load({ params }) {
+    const id = params.id
+
+      return {
+        props: {
+          id,
+        },
+      };
+  }
+</script>
+
+<script>
+  export let id;
+</script>
+
+<h1>{ id }</h1>

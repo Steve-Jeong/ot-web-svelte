ffa8cfe89dd146a525c01cea3d175ce57a8b892b
Steve Jeong <jst0930@gmail.com>
2022-04-04 22:06:16 +0300

    day2. clicking items do not show explanation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,43 @@
 <script>
-  let content = ''
+  let content = {
+    mode: "read",
+    dictionary: [
+      { id: 1, title: "HTML", desc: "HTML is ..." },
+      { id: 2, title: "CSS", desc: "CSS is ..." },
+      { id: 3, title: "JavaScript", desc: "JavaScript is ..." },
+    ],
+    selectedId: 2,
+    maxId: 3,
+  };
 
+  let explanation = "";
+  let item = {};
+
+  $: {
+    if (content.mode === "read") {
+      item = content.dictionary.find((dict) => dict.id === content.selectedId);
+      explanation = `<h2>${item.title}</h2><p>${item.desc}</p>`;
+    }
+  }
+
+  const handleId = (e) => {
+    content.selectedId = e.target.dataset.item;
+    console.log(content.selectedId);
+    content.mode = "read";
+    content = content;
+  };
 </script>
 
 <ul>
-  <li><a href="/dict/html">HTML</a></li>
-  <li><a href="/dict/css">CSS</a></li>
-  <li><a href="/dict/javascript">JavaScript</a></li>
+  {#each content.dictionary as dict (dict.id)}
+    <li>
+      <a
+        href="/dict/${dict.title.toLowerCase()}"
+        data-item={dict.id}
+        on:click|preventDefault={(e) => handleId(e)}>{dict.title}</a
+      >
+    </li>
+  {/each}
 </ul>
 
-<h2>Welcome</h2>
-<p>Welcome to the WEB Dictionary</p>
-
+<p>{@html explanation}</p>
